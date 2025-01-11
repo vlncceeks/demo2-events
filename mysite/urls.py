536d2c5461ce2257c61320a1e5945ceb7fe3f9e1
Ксения <vlncceeks@gmail.com>
2025-01-11 22:11:34 +0300

    проверка railway 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]
+    path('users/', include('users_registration.urls')),  # Подключение маршрутов из приложения
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
