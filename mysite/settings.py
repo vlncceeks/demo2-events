ed08b6292ee8d91c84d2fc43ddd541db295a52c6
Ксения <vlncceeks@gmail.com>
2025-01-12 01:26:34 +0300

    отправка формы на railway

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,11 +26,13 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['demo2-events-production.up.railway.app', 'yourdomain.com']
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-# CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://demo2-events-production.up.railway.app',
+]
 
 # Application definition
 
