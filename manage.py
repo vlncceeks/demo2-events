44dbb88dae767d2de1eaa69c5e79022430a23d04
Ксения <vlncceeks@gmail.com>
2025-01-12 16:22:02 +0300

    del script for superuser

--- manage.py
+++ manage.py
@@ -9,14 +9,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
-        import django
-        # Инициализация Django
-        django.setup()
-        from django.contrib.auth.models import User
-        superuser = 'ADMIN'
-        # Создание суперпользователя
-        if not User.objects.filter(username=superuser).exists():
-            User.objects.create_superuser(superuser, 'admin@example.com', 'adminpassword')
+        
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
