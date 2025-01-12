ac7a4705a220ae477251a04920e6996f14e1e946
Ксения <vlncceeks@gmail.com>
2025-01-12 18:12:06 +0300

    create_superuser2

--- create_superuser.py
+++ create_superuser.py
@@ -13,14 +13,14 @@ from django.contrib.auth.models import User
 
 
 
-superuser = 'Adminn'
+superuser = 'ADMIN'
 
 
 
 
 # Создание суперпользователя
 if not User.objects.filter(username=superuser).exists():
-    User.objects.create_superuser(superuser, 'admin@example.com', 'adminpassword')
+    User.objects.create_superuser(superuser, 'admin@example.com', 'hMGA9OLntQ')
     print("Суперпользователь создан успешно")
 else:
     print("Суперпользователь уже существует")
